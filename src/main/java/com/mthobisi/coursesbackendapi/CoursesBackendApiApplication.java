e230e252db22c7a3bbab8f237c82e89e86fa7a4d
Mthobisi Kunene <mthobisi.kunene@bsg.co.za>
2024-05-11 18:11:09 +0200

    Initial commit

---
+++ src/main/java/com/mthobisi/coursesbackendapi/CoursesBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.mthobisi.coursesbackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mthobisi/coursesbackendapi/CoursesBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mthobisi.coursesbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
